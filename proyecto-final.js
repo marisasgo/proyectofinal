ff81592262f814b1cd20fa5d9fe50467cf57b2ae
marisasgo <126423020+marisasgo@users.noreply.github.com>
2023-02-26 19:12:31 -0300

    Add files via upload

---
+++ proyecto-final.js
@@ -0,0 +1,5 @@
+
+document.getElementById('boton').onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("demo").innerHTML = "Gracias por visitar el CV";
+}
